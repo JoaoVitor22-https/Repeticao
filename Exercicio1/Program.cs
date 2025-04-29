d18886d0ebfd84fab260e5721f518bb2138542bd
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-04-29 11:36:31 -0300

    Comecei

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+
+       int contador = 0; 
+       while (contador < 5)
+        {
+            Console.WriteLine("Esta é a frase impressa");
+            contador++;
+
+        }
+
+       
+    }
+}
+
+
+
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Digite um número: ");
+        int informacao = Convert.ToInt32(Console.ReadLine());
+        
+        while (informacao >= 1)
+        {
+            Console.WriteLine(informacao++);
+            
+
+        }
+
+
+    }
+}
+
+
+
